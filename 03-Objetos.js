9de3328e79e2f48e91f656fb6877823e742967e3
douglaslegramante <douglas.legramante@ifro.edu.br>
2024-02-28 20:05:16 -0400

    Objetos

---
+++ 03-Objetos.js
@@ -0,0 +1,22 @@
+//Criando um objeto literal
+var produto = {
+    id: 9,
+    nome: "Cafeteira Elétrica",
+    valor: 99.00
+};
+
+//Recuperando os dados do objeto
+console.log(produto.id); // 9
+console.log(produto.nome); // Cafeteira Elétrica
+console.log(produto.valor); // 99.00
+
+//Criando um objeto literal
+var cliente = {
+    id: 40,
+    nome: "Jorge Mendes",
+    telefone: "(21) 9999-9999"
+};
+
+//Recuperando os dados do objeto
+console.log(cliente.id); // 40
+console.log(cliente.nome); // Jorge Mendes
