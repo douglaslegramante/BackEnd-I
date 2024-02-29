2ba91f135a8ff0f315bd38449769ea09e098dfb7
Douglas Legramante <douglas.legramante@ifro.edu.br>
2024-02-28 20:53:33 -0400

    Tipos de dados

---
+++ 02-TiposDeDados.js
@@ -0,0 +1,18 @@
+//Tipos de dados 
+//Number.
+//Boolean
+//String.
+
+//O tipo de uma variável é definido de acordo com o valor dela. 
+//Para descobrir o tipo de uma variável sem ser pelo seu valor, basta utilizar 'typeof'.
+var valor = 4;
+console.log(typeof valor);
+// Vai imprimir 'number'
+
+var sobrenome = "Silva";
+console.log(typeof sobrenome);
+// Vai imprimir 'string'
+
+var ligado = true;
+console.log(typeof ligado);
+// Vai imprimir 'boolean'
