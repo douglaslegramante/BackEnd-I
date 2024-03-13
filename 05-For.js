a97d37e66a9d6ab4e62b6601581c1e3fa6f5e80e
Douglas Legramante <douglas.legramante@ifro.edu.br>
2024-03-13 18:09:30 -0400

    Estrutura for

---
+++ 05-For.js
@@ -0,0 +1,34 @@
+//Sintaxe basica do for
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
+
+//Exemplo tabuada
+let multiplicador = 8;
+
+console.log("Tabuada de multiplicação do Nº " + multiplicador);
+
+for (let contador = 0; contador <= 10; contador++) {
+  let resultado = multiplicador * contador;
+  console.log(multiplicador + " x " + contador + " = " + resultado);
+}
+
+//Exemplo meses
+let meses = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+for (let contador = 0; contador < meses.length; contador++) {
+  console.log(meses[contador]);
+}
